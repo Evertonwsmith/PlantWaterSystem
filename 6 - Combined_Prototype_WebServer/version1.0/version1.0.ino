7ea57fc805cd8d8130e0667c32ebb86f18c26108
Everton <evertonwsmith@gmail.com>
2026-06-08 21:56:49 -0600

    working V1 added

--- 6 - Combined_Prototype_WebServer/version1.0/version1.0.ino
+++ 6 - Combined_Prototype_WebServer/version1.0/version1.0.ino
@@ -2,11 +2,19 @@
 #include <WiFi.h>
 #include <WebServer.h>  // Standard, stable library
 #include "index.h"
+#include <DHT.h>
+#define DHTPIN 25      // The digital pin connected to the DHT11
+#define DHTTYPE DHT11  // Define the sensor type
+
+DHT dht(DHTPIN, DHTTYPE);
 WebServer server(80);
 
 const char* ssid = "WIFI-AE90";
 const char* password = "below9825candid";
-
+const int SENSOR_PIN = 32;
+const int RELAY_TRIG = 27;
+const int SENSOR_PWR = 33;
+const int TEMP_PWR = 26;
 const char* PARAM_MESSAGE = "message";
 
 void handleNotFound() {
@@ -15,6 +23,14 @@ void handleNotFound() {
 
 void setup() {
   Serial.begin(115200);
+  pinMode(SENSOR_PWR, OUTPUT);
+  digitalWrite(SENSOR_PWR, HIGH);
+  pinMode(TEMP_PWR, OUTPUT);
+  digitalWrite(TEMP_PWR, HIGH);
+  pinMode(SENSOR_PIN, INPUT);
+  pinMode(RELAY_TRIG, OUTPUT);
+  digitalWrite(RELAY_TRIG, HIGH);
+  dht.begin();
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
@@ -30,19 +46,19 @@ void setup() {
   });
   server.on("/temperature", HTTP_GET, []() {
     // Replace 24.5 with your actual sensor reading variable (e.g., dht.readTemperature())
-    float currentTemp = 24.5;
+    float currentTemp = dht.readTemperature();
     server.send(200, "text/plain", String(currentTemp) + " °C");
   });
 
   server.on("/humidity", HTTP_GET, []() {
     // Replace 60.0 with your actual sensor reading variable (e.g., dht.readHumidity())
-    float currentHumidity = 60.0;
+    float currentHumidity = dht.readHumidity();
     server.send(200, "text/plain", String(currentHumidity) + " %");
   });
 
   server.on("/moisture", HTTP_GET, []() {
     // Replace 45 with your actual soil moisture reading variable
-    int currentMoisture = 45;
+    int currentMoisture = analogRead(SENSOR_PIN);
     server.send(200, "text/plain", String(currentMoisture) + " %");
   });
 
@@ -52,6 +68,9 @@ void setup() {
   server.on("/water-now", HTTP_POST, []() {
     Serial.println("Action: Watering started via web control!");
     // Add your relay/pump code here
+    digitalWrite(RELAY_TRIG, LOW);
+    delay(2000);
+    digitalWrite(RELAY_TRIG, HIGH);
     server.send(200, "text/plain", "Watering cycle triggered");
   });
 
@@ -72,5 +91,5 @@ void setup() {
 
 void loop() {
   server.handleClient();  // This takes care of processing incoming requests safely
-  updateData();
+  // updateData();
 }
